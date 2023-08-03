93b7247c8e72f4f0030d5b55d51634e4276827b4
eslamawd <eeslamawood@gmail.com>
2023-08-03 11:32:11 +0300

    my apdate

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "https://blog-api.onrender.com",
+    baseURL: "https://blog-api-61qi.onrender.com",
 });
 
 export default request;
